1ccde6ac2ceb5bd2901a3ea3a9c7a24fd2a5a169
Francisco Florio <francisco.florio25@gmail.com>
2023-11-10 10:51:17 -0300

    identity entities added and enum of roles

---
+++ TruckDemo-v1.Domain/Entities/Identity/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckDemo_v1.Domain.Entities.Identity
+{
+    public class UserLogin : IdentityUserLogin<Guid>
+    {
+    }
+}
---
+++ TruckDemo-v1.Domain/Enum/RoleName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckDemo_v1.Domain.Enum
+{
+    public enum RoleName
+    {
+        Basic,
+        Reader,
+        Admin
+    }
+}
