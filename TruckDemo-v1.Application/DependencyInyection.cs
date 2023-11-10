ffbd20d750f51fa7d1e0b8bee225ee18526dd94f
Lautaro Carro <lcarro@algeiba.com>
2023-11-10 12:15:13 -0300

    fix namespaces

--- TruckDemo-v1.Application/DependencyInyection.cs
+++ TruckDemo-v1.Application/DependencyInyection.cs
@@ -1,7 +1,16 @@
-﻿namespace TruckDemo_v1.Application
-{
-    public class DependencyInyection
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
 
+using TruckDemo_v1.Application.DTO.Result;
+
+namespace TruckDemo_v1.Application
+{
+    public static class DependencyInyection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(o => o.RegisterServicesFromAssembly(typeof(Result).Assembly));
+
+            return services;
+        }
     }
 }
--- TruckDemo-v1.Infrastructure/DependencyInyection.cs
+++ TruckDemo-v1.Infrastructure/DependencyInyection.cs
@@ -2,13 +2,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Data;
+
 using TruckDemo_v1.Application.Data;
 using TruckDemo_v1.Application.Managers;
 using TruckDemo_v1.Domain.Entities.Identity;
-using TruckDemo_v1.Domain.Enum;
-using TruckDemo_v1.Infraestructure.Data;
 using TruckDemo_v1.Infraestructure.Managers;
+using TruckDemo_v1.Infrastructure.Data;
 
 namespace TruckDemo_v1.Infraestructure
 {
@@ -20,7 +19,7 @@ namespace TruckDemo_v1.Infraestructure
             services.AddDbContext<ITruckDemoContext, TruckDemoContext>
                 (o => o.UseSqlServer(configuration["SqlServerConnectionString"]));
 
-            services.AddIdentity<ApplicationUser,Role>(options => options.SignIn.RequireConfirmedAccount = true)            
+            services.AddIdentity<ApplicationUser, Role>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<TruckDemoContext>().AddDefaultTokenProviders();
             services.AddScoped<IJwtManager, JwtManager>();
 
